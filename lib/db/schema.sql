80a7f361e1b5550ec0ef1d35467c35e58794d9b5
SV <72838881+santis1001@users.noreply.github.com>
2023-06-10 14:15:52 -0600

    inlcuded devolep files

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,2 +1,2 @@
-DROP DATABASE IF EXISTS traveller_db;
-CREATE DATABASE traveller_db;
+DROP DATABASE IF EXISTS ecommerce_db;
+CREATE DATABASE ecommerce_db;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routes = require('./routes');
-const sequelize = require('./config/connection');
+// import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -10,6 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
 
-sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+// sync sequelize models to the database, then turn on the server
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}!`);
 });
